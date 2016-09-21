e22ecd768cac26efbdc25f402aa842dc283aed75
feifei <phphome@qq.com>
2016-09-21 23:13:53 +0800

    更新开发说明

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -6,7 +6,7 @@ return [
             'class' => 'yii\db\Connection',
             'dsn' => 'mysql:host=localhost;dbname=yii2admin',
             'username' => 'root',
-            'password' => '',
+            'password' => 'root',
             'charset' => 'utf8',
             'tablePrefix' => 'yii2_',
         ],
