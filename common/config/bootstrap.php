1a4ebd86e089036412b95188262be85ac9199d53
bin <809870131@qq.com>
2016-09-27 08:33:46 +0800

    update

--- common/config/bootstrap.php
+++
@@ -1,5 +0,0 @@
-<?php
-Yii::setAlias('@common', dirname(__DIR__));
-Yii::setAlias('@home', dirname(dirname(__DIR__)) . '/home');
-Yii::setAlias('@backend', dirname(dirname(__DIR__)) . '/backend');
-Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
