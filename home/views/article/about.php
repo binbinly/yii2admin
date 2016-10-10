a4aacff1d28c7f289c44f4500dd895df4669d535
eric <104044924@qq.com>
2016-10-10 10:35:25 +0800

    modify

--- home/views/article/about.php
+++ home/views/article/about.php
@@ -3,8 +3,7 @@
 ?>
 
 <!--banner-->
-<div id="carousel-example-generic" class="carousel slide ban" data-ride="carousel">
-    <!-- Indicators -->
+<!-- <div id="carousel-example-generic" class="carousel slide ban" data-ride="carousel">
     <ol class="carousel-indicators">
         <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
         <li data-target="#carousel-example-generic" data-slide-to="1"></li>
@@ -21,7 +20,6 @@
             <img src="/bootstrap/images/banner.jpg" alt="Third slide">
         </div>
     </div>
-    <!-- 轮播（Carousel）导航 -->
     <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
         <span class="glyphicon-chevron-left" aria-hidden="true"><img src="/bootstrap/images/left.png"></span>
         <span class="sr-only">Previous</span>
@@ -30,7 +28,7 @@
         <span class="glyphicon-chevron-right" aria-hidden="true"><img src="/bootstrap/images/right.png"></span>
         <span class="sr-only">Next</span>
     </a>
-</div>
+</div> -->
 
 <div class="w_1200">
     <ol class="breadcrumb">
