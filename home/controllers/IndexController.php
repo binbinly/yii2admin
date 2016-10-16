1ac57e44604edf57cecbc366a55fe8bd61630fcd
eric <104044924@qq.com>
2016-10-16 10:22:49 +0800

    修改广告封面

--- home/controllers/IndexController.php
+++ home/controllers/IndexController.php
@@ -4,6 +4,7 @@ namespace home\controllers;
 
 use home\models\ShopGroup;
 use home\models\Article;
+use home\models\Ad;
 use yii\web\Controller;
 
 class IndexController extends Controller
@@ -13,6 +14,8 @@ class IndexController extends Controller
         /* 首页 套餐 幻灯片 */
         $group_ppt = ShopGroup::find()->where(['status'=>1])->orderBy('SORT desc')->limit(3)->asArray()->all();
         $article_ppt = Article::lists(3);
+        $ad_list = Ad::find()->where(['status'=>1, 'type'=>1])->all();
+        $ad2_list = Ad::find()->where(['status'=>1, 'type'=>2])->all();
         $recommd = array();
         $recommd['all'] = array_merge($group_ppt, $article_ppt);
         $recommd['active'] = $article_ppt;
@@ -22,6 +25,8 @@ class IndexController extends Controller
         return $this->render('index',[
             'group_ppt' => $group_ppt,
             'recommd' => $recommd,
+            'ad_list' => $ad_list,
+            'ad2_list' => $ad2_list,
         ]);
     }
 }
