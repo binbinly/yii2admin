9c46ae6a71544fa36eec161947979c771285b89b
eric <104044924@qq.com>
2016-10-16 23:55:40 +0800

    修改草的问题

--- backend/views/public/_image.php
+++ backend/views/public/_image.php
@@ -7,7 +7,7 @@ if (!empty($data)) {
 }
 ?>
 <style>
-.fileupload-item {width: 100px; height: 100px;position: relative;margin-right:5px;}
+.fileupload-item {width: 100px; min-height: 100px;position: relative;margin-right:5px;}
 .fileupload-del {position: absolute;bottom:5px;left:40%;cursor: pointer;}
 .fileupload-text {color: #f00;}
 </style>
